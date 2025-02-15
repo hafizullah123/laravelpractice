ea7f4d00164f7514e4c36579361367de685221ae
Hafizullah <hjihadwal@gmail.com>
2025-02-14 20:21:31 -0800

    this is the laravel route

---
+++ MyBlog/routes/web.php
@@ -0,0 +1,22 @@
+<?php
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/about', function () {
+  return  "wellcom to the about page this paratical work of the laravel framwork inshaullah i will make the project as will  ";
+});
+
+Route::post('/', function (Request $request ) {
+    dd($request ->all());
+    return view('welcome');
+});
+
+Route::put('/', function (Request $request ) {
+    dd($request ->all());
+   
+});
