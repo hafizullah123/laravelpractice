28b516d3c458d09b9c8751ba9b465e423fedda96
Hafizullah <hjihadwal@gmail.com>
2025-02-25 17:21:59 +0430

    complete the crate form page front of this code

--- MyBlog/resources/views/posts/create.blade.php
+++ MyBlog/resources/views/posts/create.blade.php
@@ -1,5 +1,17 @@
 <x-layout>
-    <h1>create page </h1>
-    <p>post1</p>
-    <p>post2</p>
-     </x-layout>
+    <div class="container mx-auto p-6 max-w-3xl bg-white shadow-md rounded-lg dark:bg-gray-900">
+        <h1 class="text-xl font-semibold text-gray-900 dark:text-white mb-4">Create Post</h1>
+
+        <form>
+            <div class="mb-6">
+                <label for="text" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title:</label>
+                <input type="text" id="text" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Enter title" required />
+
+                <label for="message" class="block mt-4 mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+                <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+
+                <button type="submit" class="mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+            </div>
+        </form>
+    </div>
+</x-layout>
